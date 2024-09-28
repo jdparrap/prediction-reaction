89482c2050fc6f0f98441a9fd51b4a726c482587
jdparrap <160335312+jdparrap@users.noreply.github.com>
2024-09-28 00:32:27 +0000

    firts ver

--- docs/reaction.js
+++ docs/reaction.js
@@ -1,21 +1,7 @@
 function getReactionText(prediction) {
-    const isFire = prediction[2].probability >= 0.50;
-    const othersAboveThreshold = prediction.some((pred, index) => index !== 2 && pred.probability > 0.50);
-
-    if (isFire && !othersAboveThreshold) {
-        return "AUXILIO FUEGO";
-    } else if (!isFire && othersAboveThreshold) {
-        return "UFFF TODO TRANQUI";
-    }
-    return "SIN DATOS RELEVANTES";
+  
 }
 
 function reaction2prediction(prediction) {
-    const fireElement = document.getElementById("txt-reaction");
-    fireElement.textContent = getReactionText(prediction);
 
-    const bodybg = document.getElementsByTagName("body")[0];
-    bodybg.style.backgroundColor = prediction[2].probability >= 0.50 ? "red" : "unset";
-
-    fireElement.textContent = getReactionText(prediction);
 }
